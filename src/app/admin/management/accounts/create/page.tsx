50bf897217506e28ce1d39e69703508cabc38407
felipscarvalho <felipecarvalho5520@gmail.com>
2024-04-25 18:28:23 -0300

    ✨ Create account page added (working)

--- src/app/admin/management/accounts/create/page.tsx
+++ src/app/admin/management/accounts/create/page.tsx
@@ -1,7 +1,16 @@
+import styles from "./CreateAccount.module.css"
+
+import CreateAccountForm from "./Form"
+
 export default async function CreateAccount() {
 	return (
-		<>
-			<h1>Create account!</h1>
-		</>
+		<main className={styles.container}>
+			<section className={styles.sectionContainer}>
+				<div className={styles.header}>
+					<h1>Crie uma Conta</h1>
+				</div>
+				<CreateAccountForm></CreateAccountForm>
+			</section>
+		</main>
 	)
 }
